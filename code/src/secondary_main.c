bb84075a24f6670dd3cb09a0b37a330db9b07247
andhmak <sdi1900047@di.uoa.gr>
2022-01-15 19:05:06 +0200

    +

--- code/src/secondary_main.c
+++ code/src/secondary_main.c
@@ -5,11 +5,12 @@
 #include "bf.h"
 #include "sht_file.h"
 
-#define RECORDS_NUM 128 // you can change it if you want
+#define RECORDS_NUM 64 // you can change it if you want
 #define GLOBAL_DEPT_1 2 // you can change it if you want
-#define GLOBAL_DEPT_2 2 // you can change it if you want
-#define FILE_NAME_1 "data_1.db"
-#define FILE_NAME_2 "data_2.db"
+#define GLOBAL_DEPT_2 1 // you can change it if you want
+#define FILE_NAME_1 "data_4.db"
+#define FILE_NAME_2 "data_5.db"
+#define FILE_NAME_3 "data_6.db"
 
 const char* names[] = {
   "Yannis",
@@ -23,7 +24,6 @@ const char* names[] = {
   "Konstantina",
   "Theofilos",
   "Giorgos",
-  "Dimitris"
 };
 
 const char* surnames[] = {
@@ -69,25 +69,30 @@ int main() {
 
   printf("Create the primary file\n");
   CALL_OR_DIE(HT_CreateIndex(FILE_NAME_1, GLOBAL_DEPT_1));
-  printf("Create the secondary file\n");
-  CALL_OR_DIE(SHT_CreateSecondaryIndex(FILE_NAME_2, "surname", 20, GLOBAL_DEPT_2, FILE_NAME_1));
+  printf("Create the surname secondary file\n");
+  //CALL_OR_DIE(SHT_CreateSecondaryIndex(FILE_NAME_2, "surname", 20, GLOBAL_DEPT_2, FILE_NAME_1));
+  printf("Create the city secondary file\n");
+  CALL_OR_DIE(SHT_CreateSecondaryIndex(FILE_NAME_3, "city", 20, GLOBAL_DEPT_2, FILE_NAME_1));
   printf("Open the primary file\n");
   int indexDesc1;
   CALL_OR_DIE(HT_OpenIndex(FILE_NAME_1, &indexDesc1));
-  printf("Open the secondary file\n");
+  printf("Open the surname secondary file\n");
   int indexDesc2;
-  CALL_OR_DIE(SHT_OpenSecondaryIndex(FILE_NAME_2, &indexDesc2));
+  //CALL_OR_DIE(SHT_OpenSecondaryIndex(FILE_NAME_2, &indexDesc2));
+  printf("Open the city secondary file\n");
+  int indexDesc3;
+  CALL_OR_DIE(SHT_OpenSecondaryIndex(FILE_NAME_3, &indexDesc3));
   Record record;
   SecondaryRecord secRecord;
   srand(12569874);
   int r;
   tTuple rec_pos;
   UpdateRecordArray updateArray;
-  printf("Insert Entries with different IDs on the first file\n");
+  printf("Insert Entries with different IDs on the primary and secondary files\n");
   for (int id = 0; id < RECORDS_NUM; ++id) {
     // create a record
     record.id = id;
-    r = id % 12;
+    r = id % 11;
     memcpy(record.name, names[r], strlen(names[r]) + 1);
     r = id % 12;
     memcpy(record.surname, surnames[r], strlen(surnames[r]) + 1);
@@ -99,37 +104,52 @@ int main() {
     secRecord.tupleId.record_num = rec_pos.record_num;
     CALL_OR_DIE(SHT_SecondaryInsertEntry(indexDesc2, secRecord));
     CALL_OR_DIE(SHT_SecondaryUpdateEntry(indexDesc2, &updateArray));
+    CALL_OR_DIE(SHT_SecondaryInsertEntry(indexDesc3, secRecord));
+    CALL_OR_DIE(SHT_SecondaryUpdateEntry(indexDesc3, &updateArray));
     free(updateArray.record);
     free(updateArray.newTuple);
     free(updateArray.oldTuple);
   }
   
-  printf("RUN PrintAllEntries with specific ID on the first file\n");
-  int id = rand() % RECORDS_NUM;
-  CALL_OR_DIE(HT_PrintAllEntries(indexDesc1, &id));
-  printf("RUN PrintAllEntries with specific ID on the secondary file\n");
-  CALL_OR_DIE(SHT_PrintAllEntries(indexDesc2, "Ioannidis"));
-  for (int i = 0; i < RECORDS_NUM; i++) {
-    CALL_OR_DIE(HT_PrintAllEntries(indexDesc1, &i));
-  }
-  printf("RUN PrintAllEntries without ID on the first file\n");
-  CALL_OR_DIE(HT_PrintAllEntries(indexDesc1, NULL));
-  printf("RUN PrintAllEntries without ID on the secondary file\n");
+  printf("RUN PrintAllEntries with specific key on the surname secondary file\n");
+  //CALL_OR_DIE(SHT_PrintAllEntries(indexDesc2, "Ioannidis"));
+  
+  printf("RUN PrintAllEntries without key on the surname secondary file\n");
   CALL_OR_DIE(SHT_PrintAllEntries(indexDesc2, NULL));
+  
+  printf("RUN PrintAllEntries with specific key on the city secondary file\n");
+  //CALL_OR_DIE(SHT_PrintAllEntries(indexDesc3, "Athens"));
+  
+  printf("RUN PrintAllEntries without key on the city secondary file\n");
+  CALL_OR_DIE(SHT_PrintAllEntries(indexDesc3, NULL));
 
-  printf("RUN HashStatistics on the first open file\n");
-  CALL_OR_DIE(HashStatistics(FILE_NAME_1));
-  printf("RUN HashStatistics on the secondary open file\n");
-  CALL_OR_DIE(HashStatistics(FILE_NAME_2));
+  printf("RUN HashStatistics on the surname secondary open file\n");
+  //CALL_OR_DIE(HashStatistics(FILE_NAME_2));
 
-  CALL_OR_DIE(SHT_InnerJoin(indexDesc2, indexDesc2, "Ioannidis"));
+  printf("RUN HashStatistics on the city secondary open file\n");
+  CALL_OR_DIE(HashStatistics(FILE_NAME_3));
 
-  printf("Close the first file\n");
+  printf("RUN InnerJoin on the surname with specific key\n");
+  //CALL_OR_DIE(SHT_InnerJoin(indexDesc2, indexDesc2, "Ioannidis"));
+
+  printf("RUN InnerJoin on the surname without key\n");
+  //CALL_OR_DIE(SHT_InnerJoin(indexDesc2, indexDesc2, NULL));
+
+  printf("RUN InnerJoin on the city with specific key\n");
+  CALL_OR_DIE(SHT_InnerJoin(indexDesc3, indexDesc3, "Ioannidis"));
+
+  printf("RUN InnerJoin on the city without key\n");
+  CALL_OR_DIE(SHT_InnerJoin(indexDesc3, indexDesc3, NULL));
+
+  printf("Close the primary file\n");
   CALL_OR_DIE(HT_CloseFile(indexDesc1));
-  CALL_OR_DIE(SHT_CloseSecondaryIndex(indexDesc2));
-  printf("RUN HashStatistics on the first closed file\n");
-  CALL_OR_DIE(HashStatistics(FILE_NAME_1));
-  printf("RUN HashStatistics on the secondary closed file\n");
+  printf("Close the surname secondary file\n");
+  //CALL_OR_DIE(SHT_CloseSecondaryIndex(indexDesc2));
+  printf("Close the city secondary file\n");
+  CALL_OR_DIE(SHT_CloseSecondaryIndex(indexDesc3));
+  printf("RUN HashStatistics on the surname secondary closed file\n");
+  //CALL_OR_DIE(HashStatistics(FILE_NAME_2));
+  printf("RUN HashStatistics on the city secondary closed file\n");
   CALL_OR_DIE(HashStatistics(FILE_NAME_2));
   BF_Close();
 }
